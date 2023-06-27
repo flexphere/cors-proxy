c99e86a17e2bf1afe8bf49690b7bc4f80b6b5cf8
ankhllc <131894976+ankhllc@users.noreply.github.com>
2023-06-28 02:28:38 +0900

    init

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+var port string
+var target string
+
+func init() {
+	flag.StringVar(&port, "p", "30080", "port to serve on")
+	flag.StringVar(&target, "t", "", "proxy target url eg:http://example.com")
+	flag.Parse()
+
+	if target == "" {
+		flag.Usage()
+		log.Fatalln("ERR: target url must be set")
+	}
+}
+
+func main() {
+	targetURL, err := url.Parse(target)
+	if err != nil {
+		log.Fatal("Failed to parse target URL:", err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+
+	proxy.Transport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+	}
+
+	proxy.Director = func(req *http.Request) {
+		req.Host = targetURL.Host
+		req.URL.Host = targetURL.Host
+		req.URL.Scheme = targetURL.Scheme
+		req.Header.Set("Connection", "keep-alive")
+	}
+
+	proxy.ModifyResponse = func(resp *http.Response) error {
+		log.Printf("[%d] %s\n", resp.StatusCode, resp.Request.URL.String())
+		resp.Header.Set("Access-Control-Allow-Origin", "*")
+		resp.Header.Set("Access-Control-Allow-Headers", "*")
+		resp.Header.Set("Access-Control-Allow-Credentials", "true")
+		resp.Header.Set("Access-Control-Allow-Methods", "*")
+		return nil
+	}
+
+	log.Printf("Redirecting http://localhost:%s -> %s\n", port, target)
+	err = http.ListenAndServe(":"+port, proxy)
+	if err != nil {
+		log.Fatal("Failed to start proxy server:", err)
+	}
+}
